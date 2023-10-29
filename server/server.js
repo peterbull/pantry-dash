71a96d5eb522687a474a86cc702da692abed2e9b
LAPTOP-VCGCOL4V <petercbull@gmail.com>
2023-10-29 15:39:24 -0400

    update route

--- server/server.js
+++ server/server.js
@@ -13,11 +13,13 @@ app.use(cors());
 app.use(express.json()); //req.body
 
 
-
+// landing page
 app.get("/", async (req, res) => {
     res.send("Landing Page");
 });
 
+
+// get all items
 app.get("/items", async (req, res) => {
     try {
         const allItems = await pool.query(
@@ -30,15 +32,36 @@ app.get("/items", async (req, res) => {
     }
 });
 
+// get an item
+app.get("/items/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const item = await pool.query(
+            "SELECT * FROM items WHERE id = $1",
+            [id]
+        );
+
+        res.json(item.rows[0]);
+    } catch (err) {
+        console.error(err.message);
+    }
+})
+
 app.post("/items", async (req, res) => {
     try {
         const { name } = req.body;
         const newItem = await pool.query(
-            "INSERT INTO items (name, quantity, ) ")
+            "INSERT INTO items (name) VALUES ($1) RETURNING *",
+            [name]
+        );
+
+        res.json(newItem.rows[0])
     } catch (err) {
         console.error(err.message);
     }
 });
+
+
 
 
 app.listen(port, () => {
