691c346d2baa1068dd41e2f93bab870d25fc8b67
LAPTOP-VCGCOL4V <petercbull@gmail.com>
2023-10-30 11:58:45 -0400

    WIP add field change logic

--- client/src/components/EditItem.js
+++ client/src/components/EditItem.js
@@ -2,20 +2,32 @@ import React, { Fragment, useState } from "react";
 
 const EditItem = ({ item }) => {
 	const [isEditable, setIsEditable] = useState([]);
-
+	const [fields, setFields] = useState({
+		name: item.name,
+		quantity: item.quantity,
+		low_quantity: item.low_quantity
+	});
 
 	const toggleEditable = () => {
 		setIsEditable(!isEditable);
 	};
 
-	
+	const handleInputChange = (e) => {
+		const { name, value } = e.target;
+		setFields({
+			...fields,
+			[name]: value,
+		});
+	};
 
 
-	return(
+
+
+	return (
 		<Fragment>
 			<td></td>
 		</Fragment>
-	)
+	);
 };
 
 
