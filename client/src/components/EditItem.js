169a4bf3363ea636e72d25bc54e60b2937b9892f
LAPTOP-VCGCOL4V <petercbull@gmail.com>
2023-10-30 08:55:54 -0400

    WIP add editable logic

---
+++ client/src/components/EditItem.js
@@ -0,0 +1,25 @@
+import React, { Fragment, useState } from "react";
+
+const EditItem = ({ item }) => {
+	const [isEditable, setIsEditable] = useState([]);
+
+
+	const toggleEditable = () => {
+		setIsEditable(!isEditable);
+	};
+
+
+
+
+	return(
+		<Fragment>
+			<td></td>
+		</Fragment>
+	)
+};
+
+
+
+
+
+export default EditItem;
