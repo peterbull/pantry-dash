4878e7881f2a7e738e4d281b4a057e8fc15bc0be
LAPTOP-VCGCOL4V <petercbull@gmail.com>
2023-11-01 07:14:08 -0400

    add comments and docstring

--- client/src/components/ListItems.js
+++ client/src/components/ListItems.js
@@ -2,40 +2,55 @@ import React, { Fragment, useState, useEffect } from "react";
 import EditItem from "./EditItem";
 import CreateItem from "./CreateItem";
 
+/**
+ * Renders a table of pantry inventory items with the ability to add, edit, and delete items.
+ * @returns {JSX.Element} The ListItems component.
+ */
 const ListItems = () => {
+	// State for storing the list of items
 	const [items, setItems] = useState([]);
 
+	// Function to delete an item by ID
 	const deleteItem = async (id) => {
 		try {
+			// Sending DELETE request to the server
 			const deleteItem = await fetch(`http://localhost:5000/${id}`, {
 				method: "DELETE"
 			});
 
+			// Update the state to remove the deleted item
 			setItems(items.filter(item => item.id !== id));
 		} catch (err) {
+			// Log any errors
 			console.log(err.message);
 		}
 	};
 
+	// Function to fetch all items from the server
 	const getItems = async () => {
 		try {
+			// Sending GET request to the server
 			const response = await fetch("http://localhost:5000/items");
 			const jsonData = await response.json();
 
+			// Update the state with the fetched items
 			setItems(jsonData);
 		} catch (err) {
+			// Log any errors
 			console.error(err.message);
 		}
 	};
 
+	// Function to handle the creation of a new item
 	const handleItemCreated = async (newItem) => {
+		// Update the state to include the new item
 		setItems([newItem, ...items]);
 	};
 
+	// Fetch items when the component mounts
 	useEffect(() => {
 		getItems();
-	},
-		[]);
+	}, []);
 
 	return (
 		<Fragment>
@@ -51,8 +66,10 @@ const ListItems = () => {
 					</thead>
 					<tbody>
 						<tr>
-							<CreateItem onItemCreated={handleItemCreated}/>
+							{/* CreateItem component for adding new items */}
+							<CreateItem onItemCreated={handleItemCreated} />
 						</tr>
+						{/* Loop through items and render EditItem component for each */}
 						{items.map((item) =>
 							<tr key={item.id}>
 								<EditItem item={item} deleteItem={deleteItem} />
@@ -64,6 +81,5 @@ const ListItems = () => {
 		</Fragment>
 	);
 };
-
 
 export default ListItems;
