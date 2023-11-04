19a351cdb01b14833574e1dc0adcd64326990ecd
LAPTOP-VCGCOL4V <petercbull@gmail.com>
2023-11-04 16:43:24 -0400

    docker compose fixes

--- server/db.js
+++ server/db.js
@@ -1,15 +1,15 @@
-// require('dotenv').config();
-// const dbPass = process.env.DB_PASSWORD;
-// const dbUser = process.env.DB_USER;
-// const dbName = process.env.DB_NAME;
+require('dotenv').config();
+const dbPass = process.env.DB_PASSWORD;
+const dbUser = process.env.DB_USER;
+const dbName = process.env.DB_NAME;
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-    user: "postgres2",
-    password: "123456",
+    user: dbUser,
+    password: dbPass,
     host: 'db',
     port: 5432,
-    database: "pantry_dash"
+    database: dbName
 });
 
 module.exports = pool;
