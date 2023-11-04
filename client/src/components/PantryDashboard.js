560754fae86e613fc97b584fc25a9fd504b14aa1
LAPTOP-VCGCOL4V <petercbull@gmail.com>
2023-11-04 05:53:25 -0400

    minor linting updates

--- client/src/components/PantryDashboard.js
+++ client/src/components/PantryDashboard.js
@@ -12,21 +12,21 @@ const PantryDashboard = () => {
   return (
     <div className="container fw-light mt-4">
       {/* <div className="container mt-4"> */}
-        <div className="jumbotron">
-          <h1 className="display-4">Welcome to Pantry Dash!</h1>
-          <p className="lead">Streamline your pantry management with our intuitive digital assistant.</p>
-          <hr className="my-4" />
-          <p>Here's how to navigate your Pantry Dashboard:</p>
-          <ul className="list-group">
-            <li className="list-group-item list-group-item-action">Overview of Inventory: Check your current stock, how much you have, and when it's time to restock.</li>
-            <li className="list-group-item list-group-item-action">Add New Items: Easily add new items with their name, quantity, and 'Restock Level'.</li>
-            <li className="list-group-item list-group-item-action">Update Quantities: Directly adjust the quantities on your dashboard; changes save as you go.</li>
-            <li className="list-group-item list-group-item-action">Remove Items: Click to remove items you've used up or no longer need.</li>
-            <li className="list-group-item list-group-item-action">Search for Items: Use the search bar to quickly find items in your pantry.</li>
-            <li className="list-group-item list-group-item-action">Shopping List: See items at or below the 'Purchase More' quantity and download the list for shopping.</li>
-            <li className="list-group-item list-group-item-action">Most Purchased: View the items you restock most often in the 'Most Purchased' section.</li>
-          </ul>
-          <p className="mt-4">Stay organized and well-stocked with Pantry Dash. We're here to ensure you always have what you need, right when you need it.</p>
+      <div className="jumbotron">
+        <h1 className="display-4">Welcome to Pantry Dash!</h1>
+        <p className="lead">Streamline your pantry management with our intuitive digital assistant.</p>
+        <hr className="my-4" />
+        <p>Here's how to navigate your Pantry Dashboard:</p>
+        <ul className="list-group">
+          <li className="list-group-item list-group-item-action">Overview of Inventory: Check your current stock, how much you have, and when it's time to restock.</li>
+          <li className="list-group-item list-group-item-action">Add New Items: Easily add new items with their name, quantity, and 'Restock Level'.</li>
+          <li className="list-group-item list-group-item-action">Update Quantities: Directly adjust the quantities on your dashboard; changes save as you go.</li>
+          <li className="list-group-item list-group-item-action">Remove Items: Click to remove items you've used up or no longer need.</li>
+          <li className="list-group-item list-group-item-action">Search for Items: Use the search bar to quickly find items in your pantry.</li>
+          <li className="list-group-item list-group-item-action">Shopping List: See items at or below the 'Purchase More' quantity and download the list for shopping.</li>
+          <li className="list-group-item list-group-item-action">Most Purchased: View the items you restock most often in the 'Most Purchased' section.</li>
+        </ul>
+        <p className="mt-4">Stay organized and well-stocked with Pantry Dash. We're here to ensure you always have what you need, right when you need it.</p>
         {/* </div> */}
       </div>
 
@@ -35,7 +35,7 @@ const PantryDashboard = () => {
         <div className="col">
           <MostPurchasedItems />
           <div className="mt-4">
-          <ShoppingList />
+            <ShoppingList />
           </div>
         </div>
         <div className="col">
